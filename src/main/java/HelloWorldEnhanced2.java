65b76292f96658209d4d67634f5be2722eb019de
Melanie Steiner <jnea.steiner@gmail.com>
2026-08-28 18:13:40 -0500

    save progress

--- src/main/java/HelloWorldEnhanced2.java
+++ src/main/java/HelloWorldEnhanced2.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,14 +9,14 @@ import jakarta.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Servlet1
  */
-@WebServlet("/servlet")
-public class servlet extends HttpServlet {
+// @WebServlet("/register")
+public class HelloWorldEnhanced2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public servlet() {
+    public HelloWorldEnhanced2() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -60,6 +59,4 @@ public class servlet extends HttpServlet {
 		doGet(request, response);
 	}
 
-} HelloWorldEnhanced2 {
-    
 }
---
+++ target/firstservlet/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Meow!</h2>
+</body>
+</html>
